5b30f8e9683c1b5af7d992aff706efa3f919dffb
Jheison Alejandro Morales <jheison.morales@telefonica.com>
2020-04-10 23:15:52 -0500

    se eliminan archivos que sobran

--- server/build/controllers/indexController.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IndexController {
-    index(req, res) {
-        res.send('Hello');
-    }
-}
-const indexController = new IndexController();
-exports.default = indexController;
