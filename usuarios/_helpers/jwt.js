bdc9fc04c2baf0e82e80bd7ec9fa734310aaf758
Jheison Alejandro Morales <jheison.morales@telefonica.com>
2020-04-18 15:34:41 -0500

    se arregla error de login

--- usuarios/_helpers/jwt.js
+++ usuarios/_helpers/jwt.js
@@ -9,8 +9,8 @@ function jwt() {
     return expressJwt({ secret, isRevoked }).unless({
         path: [
             // public routes that don't require authentication
-            '/users/authenticate',
-            '/users/register'
+            '/usuarios/autenticar',
+            '/usuarios/registrar'
         ]
     });
 }
